ff9c32066e9400c183a932643d873773e2c13cb8
MagdalenaES83 <84572667+MagdalenaES83@users.noreply.github.com>
2021-10-22 11:31:57 +0200

    Update likeReducer.js

--- src/reducers/likeReducer.js
+++ src/reducers/likeReducer.js
@@ -4,16 +4,23 @@ import {
 } from "../actions";
 import { initialState } from "../store";
 
-export const likeReducer = (state = initialState.songs, action) => {
+export const likeReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_SONGS_TO_LIKED_ARRAY:
       return {
         ...state,
-        // likedSongs: state.likedSongs.includes(action.payload)
-        //   ? [...state.likedSongs]
-        //   : [...state.likedSongs, action.payload],
-        likedSongs: [...state.likedSongs, action.payload],
-      };
+        songs:{ 
+        
+          ...state.songs,
+likedSongs: state.songs.likedSongs.concat(action.payload)
+
+
+      //   }
+      //   // likedSongs: state.likedSongs.includes(action.payload)
+      //   //   ? [...state.likedSongs]
+      //   //   : [...state.likedSongs, action.payload],
+      //   likedSongs: [...state.likedSongs, action.payload],
+       }}
     case REMOVE_SONGS_FROM_LIKED_ARRAY:
       return {
         ...state,
