2a658adb0504e831e7f143d1de99deae72be1ade
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2022-03-19 17:52:11 +0000

    Changed album to functional component

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -2,16 +2,45 @@ import React from "react";
 import Song from "./Song";
 import { Row } from "react-bootstrap";
 
+// const Album =({match, location, search})=>{
+
+//   const [album, setAlbum] = useState({})
+//   const [songs, setSongs] = useState([])
+
+// let albumId = match.params.id;
+
+// let headers = new Headers({
+//   "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+//   "X-RapidAPI-Key": "222902beabmshb95a65b737cead6p1f3ac9jsn23ced94c0d20",
+// });
+
+// useEffect(()=>{
+//   try {
+//     let response = await fetch(
+//       "https://striveschool-api.herokuapp.com/api/deezer/album/" + albumId,
+//       {
+//         method: "GET",
+//         headers,
+//       }
+//     );
+
+//     if (response.ok) {
+//       let albumRes = await response.json();
+//       setAlbum(albumRes)
+//       setSongs(albumRes.tracks.data,)
+//     }
+//   } catch (exception) {
+//     console.log(exception);
+//   }
+// })
+
+// export default withRouter(Album);
+
 class Album extends React.Component {
   state = {
     album: {},
     songs: [],
   };
-
-  // const Album =()=>{
-
-  //   const [album, setAlbum] = useState({})
-  //   const [songs, setSongs] = useState([])
 
   componentDidMount = async () => {
     let albumId = this.props.match.params.id;
